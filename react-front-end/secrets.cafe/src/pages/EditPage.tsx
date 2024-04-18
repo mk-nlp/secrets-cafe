122c032c1ebd35a11bcb0291fec73f656cec68b8
Yuya Iwatsuki <spidywebcoder@gmail.com>
2024-04-18 14:46:09 +0900

    changed style and remove Navbar and Footer since it's duplicated

--- react-front-end/secrets.cafe/src/pages/EditPage.tsx
+++ react-front-end/secrets.cafe/src/pages/EditPage.tsx
@@ -1,5 +1,3 @@
-import Navbar from "@/components/Layout/TopNav/navbar";
-import Footer from "@/components/Layout/Footer/Footer";
 import Edit from "@/components/Features/components/edit";
 import { Button } from "@/components/ui/button";
 import { Link } from "react-router-dom";
@@ -7,19 +5,13 @@ import { Link } from "react-router-dom";
 
 export default function HomePage() {
     return (
-        <div className="flex flex-col min-h-screen">
-            <Navbar />
-            <main className="flex-grow flex flex-col items-center justify-center">
-                <Edit />
-                <div className="flex justify-center my-8">
-                    <Link to="/">
-                        <Button size="lg">Back</Button>
-                    </Link>
-                </div>
-            </main>
-            <footer className="mt-auto">
-                <Footer />
-            </footer>
+        <div className="flex-grow flex flex-col items-center justify-center">
+            <Edit />
+            <div className="flex justify-center my-8">
+                <Link to="/">
+                    <Button size="lg">Back</Button>
+                </Link>
+            </div>
         </div>
     );
 }
