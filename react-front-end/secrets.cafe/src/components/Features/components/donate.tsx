344dc213cbf2e562bd515779c52f0c7aac5a6bbb
Yuya Iwatsuki <spidywebcoder@gmail.com>
2024-05-14 22:52:43 +0900

    Apply design to the donate page

--- react-front-end/secrets.cafe/src/components/Features/components/donate.tsx
+++ react-front-end/secrets.cafe/src/components/Features/components/donate.tsx
@@ -1,31 +1,28 @@
-import HeartEmoji from "@/assets/heart-emoji.svg";
-import { Button } from "../../ui/button";
-
+import HeartEmoji from "@/assets/donate.svg";
+import { Button } from "@/components/ui/button";
 export default function Donate() {
-  function donate() {
-    window.open("https://ko-fi.com/secrets_cafe", "_blank");
-  }
-
-  return (
-    <div className="grid grid-cols-3 items-center justify-center gap-5 mt-7">
-      <div className="col-start-2 flex items-center justify-center">
-        <img className=" w-64" src={HeartEmoji} alt="heart emoji" />
-      </div>
-      <h1 className="col-span-3 justify-center items-center text-center text-7xl font-amatic font-medium">
-        Donate
-      </h1>
-      <h2 className="col-span-3 text-center p-3 md:col-start-2 md:col-end-3">
-        Secrets.cafe is a freely available open-source project that operates
-        without any income. If you value its existence and would like to support
-        its continuation, please consider making a donation of any amount. Your
-        contributions will go towards covering upkeep costs.
-      </h2>
-      <Button
-        onClick={donate}
-        className="col-start-2 bg-white font-amatic text-black rounded-base border-2 border-black justify-center items-center text-center text-2xl font-medium hover:bg-slate-300"
-      >
-        Donate
-      </Button>
-    </div>
-  );
+    function donate() {
+        window.open("https://ko-fi.com/secrets_cafe", "_blank");
+    }
+    return (
+        <div className="grid grid-cols-3 items-center justify-center gap-5 mt-7">
+            <div className="col-start-2 flex items-center justify-center">
+                <img className="w-[180px]" src={HeartEmoji} alt="heart emoji" />
+            </div>
+            <h1 className="col-span-3 justify-center items-center text-center font-['Open_Sans'] font-bold text-[48px] leading-[60px]">
+                Donate
+            </h1>
+            <h2 className="col-span-3 text-center p-3 md:col-start-2 md:col-end-3 font-['Open_Sans'] text-base font-normal leading-[27.2px]">
+                Secrets.cafe is a freely available open-source project that operates
+                without any income. If you value its existence and would like to support
+                its continuation, please consider making a donation of any amount. Your
+                contributions will go towards covering upkeep costs.
+            </h2>
+            <div className="col-start-2 justify-center items-center text-center">
+                <Button onClick={donate} size="lg" >
+                    Donate
+                </Button>
+            </div>
+        </div>
+    );
 }
